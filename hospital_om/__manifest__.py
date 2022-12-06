7011b92b76bce0d0ffce4cd3016c67529336b3cd
unknown <sor-panharith>
2022-12-06 23:17:08 +0700

    update addons

---
+++ hospital_om/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0',
+    'category': 'Productivity',
+    'description': 'Hospital Management Software',
+    'summary': 'Hospital Management Software',
+    'sequence': '10',
+    'author': 'Odoo Mates',
+    'company': 'Odoo Mates',
+    'maintainer': 'Odoo Mates',
+    'website': 'https://www.odoomates.tech',
+    'depends': ['sale', 'mail'],
+    'demo': [],
+    'data': [
+        'views/patient.xml',
+        'views/sale.xml',
+        'security/ir.model.access.csv',
+        'data/data.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [],
+}
